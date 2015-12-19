7d8963be538b08d0221a3d901a21e5f41d2f4c6f
Xon <tazipper1@hotmail.com>
2015-12-19 09:51:38 +0800

    Report alert support

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Some hacks to avoid XenForo design issues which trigger DB errors
 - Race condition for watch thread toggle
 - Deadlock on conversations
 - Deadlock on posts
+- Deadlock on resolving/rejecting Reports
 
 ## Requirements
 - php +5.4
@@ -24,3 +25,7 @@ Provides a workaround for a XenForo design issue where updating conversation cou
 
 Provides a workaround for a XenForo design issue where adding/removing posts can cause deadlocks.
 - updating user alert counters occurs inside a large transaction, and is deadlock prone.
+
+### Deadlock on resolving/rejecting Reports
+Provides a workaround for a XenForo design issue where adding/removing posts can cause deadlocks.
+- Sends report resolve/reject alerts inside the transaction.
---
+++ upload/library/SV/DeadlockAvoidance/XenForo/Model/Report.php
@@ -0,0 +1,28 @@
+<?php
+
+class SV_DeadlockAvoidance_XenForo_Model_Report extends XFCP_SV_DeadlockAvoidance_XenForo_Model_Report
+{
+    public function sendAlertsOnReportResolution(array $report, $comment = '')
+    {
+        if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function () use ($report, $comment)
+        {
+            parent::sendAlertsOnReportResolution($report, $comment);
+        }))
+        {
+            return;
+        }
+        parent::sendAlertsOnReportResolution($report, $comment);
+    }
+
+    public function sendAlertsOnReportRejection(array $report, $comment = '')
+    {
+        if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function () use ($report, $comment)
+        {
+            parent::sendAlertsOnReportRejection($report, $comment);
+        }))
+        {
+            return;
+        }
+        parent::sendAlertsOnReportRejection($report, $comment);
+    }
+}
