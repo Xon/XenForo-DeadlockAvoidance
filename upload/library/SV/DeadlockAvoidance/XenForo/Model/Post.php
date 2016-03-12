ea78009e0cb249f9a1069c8a9d472db754f49f76
Xon <tazipper1@hotmail.com>
2016-03-12 15:03:54 +0800

    Avoid deadlock on moving posts.

---
+++ upload/library/SV/DeadlockAvoidance/XenForo/Model/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+class SV_DeadlockAvoidance_XenForo_Model_Post extends XFCP_SV_DeadlockAvoidance_XenForo_Model_Post
+{
+    protected function _moveOrCopyPosts($action, array $posts, array $sourceThreads, array $targetThread, array $options = array())
+    {
+        if ($action != 'move')
+        {
+            return parent::_moveOrCopyPosts($action, $posts, $sourceThreads, $targetThread, $options);
+        }
+
+        // hoist bits out of the _moveOrCopyPosts Transaction
+        SV_DeadlockAvoidance_DataWriter::enterTransaction();
+        $ret = false;
+        try
+        {
+            $ret = parent::_moveOrCopyPosts($action, $posts, $sourceThreads, $targetThread, $options);
+        }
+        finally
+        {
+            SV_DeadlockAvoidance_DataWriter::exitTransaction($ret);
+        }
+        return $ret;
+    }
+}
