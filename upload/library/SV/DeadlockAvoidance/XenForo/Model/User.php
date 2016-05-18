e3dad9329051230121aa0cc479fa45d2a2b1e09c
Xon <tazipper1@hotmail.com>
2016-05-19 00:59:49 +0800

    Prevent deadlocks/races when following someone

---
+++ upload/library/SV/DeadlockAvoidance/XenForo/Model/User.php
@@ -0,0 +1,37 @@
+<?php
+
+class SV_DeadlockAvoidance_XenForo_Model_User extends XFCP_SV_DeadlockAvoidance_XenForo_Model_User
+{
+    protected function getLock($name, $timeout)
+    {
+        $db = $this->_getDb();
+        return $db->fetchOne("select get_lock(?, ?)", array($name, $timeout));
+    }
+
+    protected function releaseLock($name)
+    {
+        $db = $this->_getDb();
+        return $db->fetchOne("select release_lock(?)", array($name));
+    }
+
+    public function follow(array $followUsers, $dupeCheck = true, array $user = null)
+    {
+        if ($user === null)
+        {
+            $user = XenForo_Visitor::getInstance();
+        }
+        $key = 'follow-'.$user['user_id'];
+        if (!$this->getLock($key, 1))
+        {
+            return false;
+        }
+        try
+        {
+            return parent::follow($followUsers, $dupeCheck, $user);
+        }
+        finally
+        {
+            $this->releaseLock($key);
+        }
+    }
+}
