c780337f1722f3b6d478cbcad6a5941c8050094f
Xon <tazipper1@hotmail.com>
2016-08-31 00:44:11 +0800

    retry on like deadlock

--- upload/library/SV/DeadlockAvoidance/XenForo/Model/Like.php
+++ upload/library/SV/DeadlockAvoidance/XenForo/Model/Like.php
@@ -4,17 +4,27 @@ class SV_DeadlockAvoidance_XenForo_Model_Like extends XFCP_SV_DeadlockAvoidance_
 {
     public function likeContent($contentType, $contentId, $contentUserId, $likeUserId = null, $likeDate = null)
     {
-        if (!$contentUserId)
-        {
-            return parent::likeContent($contentType, $contentId, $contentUserId, $likeUserId, $likeDate);
-        }
-
         // hoist bits out of the Like Transaction
         SV_DeadlockAvoidance_DataWriter::enterTransaction();
         $ret = false;
         try
         {
             $ret = parent::likeContent($contentType, $contentId, $contentUserId, $likeUserId, $likeDate);
+        }
+        catch(Zend_Db_Statement_Mysqli_Exception $e)
+        {
+            SV_DeadlockAvoidance_DataWriter::exitTransaction(false);
+            @XenForo_Db::rollbackAll();
+            // something went wrong, retry
+            if (stripos($e->getMessage(), "Deadlock found when trying to get lock; try restarting transaction") !== false)
+            {
+                SV_DeadlockAvoidance_DataWriter::enterTransaction();
+                $ret = parent::likeContent($contentType, $contentId, $contentUserId, $likeUserId, $likeDate);
+            }
+            else
+            {
+                throw $e;
+            }
         }
         finally
         {
