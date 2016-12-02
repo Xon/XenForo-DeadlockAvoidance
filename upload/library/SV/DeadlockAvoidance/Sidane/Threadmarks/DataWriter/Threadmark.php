f3c36186a653ea9a49b33edf278581b8e93f2b16
Xon <tazipper1@hotmail.com>
2016-12-02 08:41:33 +0800

    Support:
    - profile posts/profile post comments
    - threadmarks

---
+++ upload/library/SV/DeadlockAvoidance/Sidane/Threadmarks/DataWriter/Threadmark.php
@@ -0,0 +1,32 @@
+<?php
+
+class SV_DeadlockAvoidance_Sidane_Threadmarks_DataWriter_Threadmark extends XFCP_SV_DeadlockAvoidance_Sidane_Threadmarks_DataWriter_Threadmark
+{
+    public function save()
+    {
+        SV_DeadlockAvoidance_DataWriter::enterTransaction();
+        $ret = false;
+        try
+        {
+            $ret = parent::save();
+            return $ret;
+        }
+        finally
+        {
+            SV_DeadlockAvoidance_DataWriter::exitTransaction($ret);
+        }
+    }
+
+    protected function _postSaveAfterTransaction()
+    {
+        if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function ()
+        {
+            parent::_postSaveAfterTransaction();
+        }))
+        {
+            return;
+        }
+
+        parent::_postSaveAfterTransaction();
+    }
+}
---
+++ upload/library/SV/DeadlockAvoidance/XenForo/DataWriter/ProfilePostComment.php
@@ -0,0 +1,44 @@
+<?php
+
+class SV_DeadlockAvoidance_XenForo_DataWriter_ProfilePostComment extends XFCP_SV_DeadlockAvoidance_XenForo_DataWriter_ProfilePostComment
+{
+    public function save()
+    {
+        SV_DeadlockAvoidance_DataWriter::enterTransaction();
+        $ret = false;
+        try
+        {
+            $ret = parent::save();
+            return $ret;
+        }
+        finally
+        {
+            SV_DeadlockAvoidance_DataWriter::exitTransaction($ret);
+        }
+    }
+
+    protected function _postSaveAfterTransaction()
+    {
+        if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function ()
+        {
+            parent::_postSaveAfterTransaction();
+        }))
+        {
+            return;
+        }
+
+        parent::_postSaveAfterTransaction();
+    }
+
+    public function _indexForSearch()
+    {
+        if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function ()
+        {
+            parent::_indexForSearch();
+        }))
+        {
+            return;
+        }
+        parent::_indexForSearch();
+    }
+}
