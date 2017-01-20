345d7fc10b9ef7fb2b37949379e75aef7ea526fc
Xon <tazipper1@hotmail.com>
2017-01-20 08:45:56 +0800

    Hoist alerts out of nested transactions.

--- upload/library/SV/DeadlockAvoidance/XenForo/Model/Alert.php
+++ upload/library/SV/DeadlockAvoidance/XenForo/Model/Alert.php
@@ -2,6 +2,18 @@
 
 class SV_DeadlockAvoidance_XenForo_Model_Alert extends XFCP_SV_DeadlockAvoidance_XenForo_Model_Alert
 {
+    public function alertUser($alertUserId, $userId, $username, $contentType, $contentId, $action, array $extraData = null)
+    {
+        if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function () use ($alertUserId, $userId, $username, $contentType, $contentId, $action, $extraData)
+        {
+            parent::alertUser($alertUserId, $userId, $username, $contentType, $contentId, $action, $extraData);
+        }))
+        {
+            return;
+        }
+        parent::alertUser($alertUserId, $userId, $username, $contentType, $contentId, $action, $extraData);
+    }
+
     public function deleteAlerts($contentType, $contentId, $userId = null, $action = null)
     {
         if (SV_DeadlockAvoidance_DataWriter::registerPostTransactionClosure(function () use ($contentType, $contentId, $userId, $action)
