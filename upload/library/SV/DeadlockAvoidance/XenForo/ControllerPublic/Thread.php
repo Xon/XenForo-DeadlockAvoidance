bd1836f219b2d99a98eefd7d9cab3727a25344de
Xon <tazipper1@hotmail.com>
2017-04-18 19:38:25 +0800

    Report Improvements reply-ban compatibility

---
+++ upload/library/SV/DeadlockAvoidance/XenForo/ControllerPublic/Thread.php
@@ -0,0 +1,19 @@
+<?php
+
+class SV_DeadlockAvoidance_XenForo_ControllerPublic_Thread extends XFCP_SV_DeadlockAvoidance_XenForo_ControllerPublic_Thread
+{
+    public function actionReplyBans()
+    {
+        SV_DeadlockAvoidance_DataWriter::enterTransaction();
+        $ret = false;
+        try
+        {
+            $ret = parent::actionReplyBans();
+            return $ret;
+        }
+        finally
+        {
+            SV_DeadlockAvoidance_DataWriter::exitTransaction(!empty($ret));
+        }
+    }
+}
